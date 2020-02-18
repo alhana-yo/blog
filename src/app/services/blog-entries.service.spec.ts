0350292c5e45049d946262c7dad214f2ba646f38
Beatriz German <beatriz.german@kairosds.com>
2020-02-18 10:00:01 +0100

    Add test for API request

--- src/app/services/blog-entries.service.spec.ts
+++ src/app/services/blog-entries.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { BlogEntriesService } from "./blog-entries.service";
 
-import { BlogEntriesService } from './blog-entries.service';
-
-describe('BlogEntriesService', () => {
+describe("BlogEntriesService", () => {
   let service: BlogEntriesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BlogEntriesService]
+    });
     service = TestBed.inject(BlogEntriesService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });

c5362a2626b53bffa3a209b89c0f8a94544c8718
Beatriz German <beatriz.german@kairosds.com>
2020-02-18 11:25:40 +0100

    Add test for the data received from de Api request

--- src/app/services/blog-entries.service.spec.ts
+++ src/app/services/blog-entries.service.spec.ts
@@ -1,5 +1,9 @@
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
+import { FAKE_POSTS } from "../constants/posts-fake.spec";
 import { BlogEntriesService } from "./blog-entries.service";
 
 describe("BlogEntriesService", () => {
@@ -15,5 +19,14 @@ describe("BlogEntriesService", () => {
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+  it("should call to server to get posts", () => {
+    const httpMock = TestBed.inject(HttpTestingController);
+    service.getPosts().subscribe(posts => expect(posts).toBe(FAKE_POSTS));
+    // De esta forma nos estamos protegiendo si alguien modifica esa url
+    const request = httpMock.expectOne("http://localhost:3000/blogEntries");
+    expect(request.request.method).toEqual("GET");
+    request.flush(FAKE_POSTS);
+    httpMock.verify();
   });
 });

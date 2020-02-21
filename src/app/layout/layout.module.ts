30b832daedac66749bf6b466c067e3e057468531
Beatriz German <beatriz.german@kairosds.com>
2020-02-21 12:05:30 +0100

    Add structure for simple component in layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { AppLayoutComponent } from "./app-layout/app-layout.component";
+import { SimpleComponent } from './simple/simple.component';
 
 @NgModule({
-  declarations: [AppLayoutComponent],
+  declarations: [AppLayoutComponent, SimpleComponent],
   imports: [CommonModule, RouterModule],
   exports: [AppLayoutComponent]
 })

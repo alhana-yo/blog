471b63faa49d2cb8257e82e22f97f49c0a4635fd
Beatriz German <beatriz.german@kairosds.com>
2020-02-27 14:29:46 +0100

    Add Psot model and request to the dataBase

--- src/app/services/new-post.service.ts
+++ src/app/services/new-post.service.ts
@@ -1,11 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HOST } from '../constants/api.constants';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewPostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  // tslint:disable-next-line: ban-types
+  savePost(postInfo: Object): Observable<any> {
+    return this.http.post(HOST + 'blogEntries', postInfo);
+  }
+
 }
 
 
